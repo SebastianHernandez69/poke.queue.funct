5fe8f860631c493664e559670fa8ec9838bb36c8
SebastianHernandez69 <sh3641046@gmail.com>
2025-04-25 09:59:15 -0600

    enriching csv report content

--- function_app.py
+++ function_app.py
@@ -7,6 +7,7 @@ import os
 import pandas as pd
 from azure.storage.blob import BlobServiceClient
 import io
+import concurrent.futures
 
 app = func.FunctionApp()
 
@@ -28,7 +29,7 @@ def QueueTriggerPokeRequest(azqueue: func.QueueMessage):
     id = record[0]["id"]
 
     update_request(id, "inprogress")
-
+    
     request = get_request(id)
     pokemons = get_pokemons(request["type"])
 
@@ -56,18 +57,23 @@ def update_request(id: int, status: str, url: str = None) -> dict:
     return response.json()
 
 def get_request(id: int) -> dict:
+    # logging.info(f"domain: {DOMAIN}")
     response = requests.get(f"{ DOMAIN }/api/request/{id}")
+    # logging.info(f"Response from get_request: {response.json()}")
     return response.json()[0]
 
 
 def get_pokemons(type: str) -> dict:
     pokeapi_url = f"https://pokeapi.co/api/v2/type/{type}"
 
-    response = requests.get(pokeapi_url, timeout=3000)
+    response = requests.get(pokeapi_url, timeout=50)
     data = response.json()
     poke_entries = data.get("pokemon", [])
 
-    return [p["pokemon"] for p in poke_entries]
+    pokemons = [p["pokemon"] for p in poke_entries]
+
+    pokemons_with_details = get_pokemon_details(pokemons)
+    return pokemons_with_details
 
 
 def generate_csv_to_blob(pokemons_list: list) -> bytes:
@@ -89,3 +95,36 @@ def upload_csv_to_blob(blob_name: str, csv_data: bytes):
     except Exception as e:
         logging.error(f"Error uploading CSV to blob: {e}")
         raise
+
+
+def get_pokemon_details(pokemons: list) -> dict:
+    def fetch_pokemon(p):
+        try:
+            response = requests.get(p["url"], timeout=10)
+            data = response.json()
+
+            stats = {s["stat"]["name"]: s["base_stat"] for s in data["stats"]}
+            abilities = ", ".join([a["ability"]["name"] for a in data["abilities"]])
+
+            return {
+                "name": p["name"],
+                "url": p["url"],
+                "hp": stats.get("hp"),
+                "attack": stats.get("attack"),
+                "defense": stats.get("defense"),
+                "special-attack": stats.get("special-attack"),
+                "special-defense": stats.get("special-defense"),
+                "speed": stats.get("speed"),
+                "abilities": abilities,
+            }
+        
+        except Exception as e:
+            logging.error(f"Error getting details for {p['name']}: {e}")
+            return None
+        
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        results = list(executor.map(fetch_pokemon, pokemons))
+
+        
+    return [result for result in results if result is not None]
+
